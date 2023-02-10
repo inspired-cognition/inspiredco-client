614abf2721bd3c45db206df02e24d1c35542d721
Graham Neubig <neubig@gmail.com>
2023-02-10 18:10:59 -0500

    Add more verbose status

--- inspiredco/critique.py
+++ inspiredco/critique.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
 import time
-from typing import Any
+from typing import Any, Literal, TypedDict
 
 from inspiredco import client_base
 
 origin = "https://critique.api.inspiredco.ai"
+
+
+class CritiqueStatus(TypedDict):
+    metric: str
+    status: Literal["queued", "succeeded", "failed"]
+    detail: str | None
 
 
 class Critique(client_base.ClientBase):
@@ -41,17 +47,23 @@ class Critique(client_base.ClientBase):
     def fetch_status(
         self,
         task_id: str,
-    ) -> str:
+    ) -> CritiqueStatus:
         """Fetch task status.
 
         Args:
             task_id: The ID of the task to fetch.
 
         Returns:
-            The status of the task including:
+            The full status of the task as a dictionary.
+
+            The "metric" key will include the name of the metric.
+
+            The "status" key will be one of the following:
             * "queued": The task is queued and possibly processing.
             * "succeeded": The task has succeeded.
             * "failed": The task has failed.
+
+            The "detail" key may include additional information.
 
         Raises:
             RuntimeError: If an error occurs.
@@ -64,9 +76,11 @@ class Critique(client_base.ClientBase):
                 f"status: {response.text}."
             )
         json_response = response.json()
-        if "status" not in json_response:
-            raise RuntimeError(f"Error in fetching task status: {json_response}.")
-        return json_response["status"]
+        return {
+            "metric": json_response["metric"],
+            "status": json_response["status"],
+            "detail": json_response.get("detail"),
+        }
 
     def fetch_result(
         self,
@@ -113,14 +127,15 @@ class Critique(client_base.ClientBase):
         # Wait
         start_time = time.time()
         while True:
-            status = self.fetch_status(task_id)
+            full_status = self.fetch_status(task_id)
+            status = full_status["status"]
             # Get the time in seconds
             total_time = time.time() - start_time
             self._logger.info(f"Status {status} at {total_time:.1f}s.")
             if status == "succeeded":
                 break
             if status == "failed":
-                raise RuntimeError("Task failed.")
+                raise RuntimeError(f"Task failed: {full_status['detail']}")
             sleep_time = 1 if total_time < 10 else 5
             time.sleep(sleep_time)
 
